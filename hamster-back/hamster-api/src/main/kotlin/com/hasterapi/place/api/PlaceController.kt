c38c6f26dd4dcd0baad781dc21e90f0d0f597ae1
moto6 <ultraraptor@naver.com>
2026-02-18 21:20:16 +0900

    bugfix : 포메팅 과정에서 빠진 임포트 원복

--- hamster-back/hamster-api/src/main/kotlin/com/hasterapi/place/api/PlaceController.kt
+++ hamster-back/hamster-api/src/main/kotlin/com/hasterapi/place/api/PlaceController.kt
@@ -1,5 +1,8 @@
 package com.hasterapi.place.api
 
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 @RestController
 @RequestMapping("/api/v0/place")
 class PlaceController {
