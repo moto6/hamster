b3e05e28fa9fa75460c096ab520fee5a664d9e03
moto6 <ultraraptor@naver.com>
2026-02-20 19:43:57 +0900

    chore : 폴리싱

--- hamster-back/hamster-api/src/main/kotlin/com/hasterapi/auth/api/dto/JwtIssueResponse.kt
+++ hamster-back/hamster-api/src/main/kotlin/com/hasterapi/auth/api/dto/JwtIssueResponse.kt
@@ -1,7 +1,8 @@
 package com.hasterapi.auth.api.dto
 
 data class JwtIssueResponse(
-    val tokenType: String = "Bearer ",
+    val tokenType: String = "Bearer",
+    val separator: String = " ",
     val accessToken: String,
     val expiresIn: Long? = null,
 )
